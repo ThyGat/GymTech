25cbe32dad6f8d12d654387ae61626409e993eb4
ThyGat <thygat@live.com>
2015-05-18 14:45:34 +0200

    date really fixed

--- index.js
+++ index.js
@@ -79,11 +79,7 @@ function createDetailsPage (res, user) {
         result += '<h1>Subscription form</h1>';
         result += '<table>';
         for (prop in user) {
-    	  	if (prop.includes("updated")) {
-    	  		result += '<tr><td class="p">'+prop+'</td><td class="v">'+user[prop]+'</td></tr>'
-        	} else {
-	            result += '<tr><td class="p">'+prop+'</td><td class="v">'+user[prop]+'</td></tr>'
-	        }
+	         result += '<tr><td class="p">'+prop+'</td><td class="v">'+user[prop]+'</td></tr>'
         }
         result += '</table>';
         result += '<p><a href="'+HOME+'"><img src="restart.png"></a></p>';
